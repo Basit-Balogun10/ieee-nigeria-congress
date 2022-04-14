c4a5be26a99fdae2a14b83235a8ec87b148dbb81
starlingvibes <starlingroot@protonmail.com>
2022-04-14 22:07:41 +0100

    updated tailwind config

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,3 @@
-tailwind.config = {
-    darkMode: 'class',
-    theme: {
-        fontFamily: {
-            poppins: ["Poppins", "sans-serif"],
-            roboto: ["Roboto", "sans-serif"]
-        },
-        colors: {
-            'darkGreen': '#328704',
-            'lightGreen': '#98E8BC',
-        },
-        extend: {},
-    }
-}
-
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 const loadTheme = () => {
     if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
@@ -21,7 +6,7 @@ const loadTheme = () => {
     document.documentElement.classList.remove('dark')
     }
 }
-window.addEventListener('load', loadTheme)
+// window.addEventListener('load', loadTheme)
 
 
 const changeToLightTheme = () => {
