1b4bd582cead0ee669ac98c6f6d6a4163765b30a
Basit-Balogun10 <basitbalogun10@gmail.com>
2022-04-15 04:10:59 +0100

    Implemented dark mode feature; implemented jump to top or bottom feature

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,67 @@
-// On page load or when changing themes, best to add inline in `head` to avoid FOUC
-const loadTheme = () => {
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark')
+const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+const topBtn = document.getElementById('toTopBtn')
+const bottomBtn = document.getElementById('toBottomBtn')
+
+window.onscroll = () => {toggleJumpBtn()}
+
+const toggleJumpBtn = () => {
+    if ((document.documentElement.scrollTop > document.documentElement.scrollHeight/3) || (document.body.scrollTop > document.body.scrollHeight/3)) {
+        !bottomBtn.classList.contains('hidden') && bottomBtn.classList.toggle('hidden');
+        topBtn.classList.contains('hidden') && topBtn.classList.toggle('hidden');
     } else {
-    document.documentElement.classList.remove('dark')
+        !topBtn.classList.contains('hidden') && topBtn.classList.toggle('hidden');
+        bottomBtn.classList.contains('hidden') && bottomBtn.classList.toggle('hidden');
     }
 }
-// window.addEventListener('load', loadTheme)
 
-
-const changeToLightTheme = () => {
-    document.documentElement.classList.tog('dark')
-    localStorage.theme = 'light'
-
-const changeToDarkTheme = () => {
-    document.documentElement.classList.add('dark')
-    localStorage.theme = 'dark'
+const jumpToTop = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
+
+const jumpToBottom = () => {
+    window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight)
+}
+
+// Change the icons inside the button based on previous settings
+if (
+    localStorage.getItem('ascf-theme') === 'dark' ||
+    (!('ascf-theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+    themeToggleLightIcon.classList.remove('hidden');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+}
+
+const themeToggleBtn = document.getElementById('theme-toggle');
+
+const toggleThemeMode = () => {
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    // if set via local storage previously
+    if (localStorage.getItem('ascf-theme')) {
+        if (localStorage.getItem('ascf-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('ascf-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('ascf-theme', 'light');
+        }
+
+        // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('ascf-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('ascf-theme', 'dark');
+        }
+    }
+}
+
+themeToggleBtn.addEventListener('click', toggleThemeMode);
